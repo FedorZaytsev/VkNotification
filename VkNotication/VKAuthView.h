b7eb852e46c4f6cdf0d98ce91ac618a4c0308248
Lobster <lobster@MacBook-Pro-Mac.local>
2014-06-08 11:25:26 +0400

    initial commit

---
+++ VkNotication/VKAuthView.h
@@ -0,0 +1,13 @@
+//
+//  VKAuthView.h
+//  VkNotication
+//
+//  Created by Lobster on 05.06.14.
+//  Copyright (c) 2014 Lobster. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VKAuthView : WebView
+
+@end
---
+++ VkNotication/VKAuthView.m
@@ -0,0 +1,13 @@
+//
+//  VKAuthView.m
+//  VkNotication
+//
+//  Created by Lobster on 05.06.14.
+//  Copyright (c) 2014 Lobster. All rights reserved.
+//
+
+#import "VKAuthView.h"
+
+@implementation VKAuthView
+
+@end
